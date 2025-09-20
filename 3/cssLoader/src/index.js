7f968ff436a35af14560324b82333a446cfcf35b
Dilip Sonowal <sonowaldilip@gmail.com>
2025-09-20 23:36:57 +0530

    Css module

---
+++ 3/cssLoader/src/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+import _ from 'lodash';
+document.getElementById("addItem").addEventListener('click', ()=> {
+    const ul = document.getElementById('shoppingList');
+
+    const listItem = ["Apple", "Banana", "Orange", "Mango"];
+
+    _.forEach(listItem, (element) => {
+        const li = document.createElement('li');
+        li.textContent = element;
+        ul.appendChild(li);
+    });
+})
